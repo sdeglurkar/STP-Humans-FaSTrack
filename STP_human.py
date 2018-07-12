b6ad9b1f5a9d2576c1e1b782cfd3e93c9a02d9f1
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2018-07-12 15:09:27 -0700

    Everything in 3D now

--- STP_human.py
+++ STP_human.py
@@ -19,9 +19,10 @@ import pedestrian_prediction.pp.inference.hardmax.state as st
 class STP_Human:
 	def __init__(self):
 		self.load_parameters()
-		rospy.init_node('human_pose_listener', anonymous=True)
+		rospy.init_node('STP', anonymous=True)
 		self.time_counter = rospy.get_time()
-		self.sub = rospy.Subscriber('human_pose', Point, self.human_callback)
+		self.human_sub = rospy.Subscriber('human_pose', Point, self.human_callback)
+		#self.occugrid_sub = rospy.Subscriber('occupancy_grid_time', OccupancyGridTime, self.occugrid_callback)
 		self.pub0 = rospy.Publisher('robot_pose0', Point, queue_size=10)
 		self.pub1 = rospy.Publisher('robot_pose1', Point, queue_size=10)
 		self.pub2 = rospy.Publisher('robot_pose2', Point, queue_size=10)
@@ -33,33 +34,32 @@ class STP_Human:
 		self.debugger_pub = rospy.Publisher('stp_debugger', String, queue_size=10)
 
 		self.static_obs = {}
-		self.static_obs[0] = []
-		self.static_obs_poses = []
-		for i in range(self.sim_length):
-			row = [False for j in range(self.sim_width)]
-			self.static_obs[0].append(row)
+		self.static_obs[0] = np.array(np.zeros((self.sim_length, self.sim_width, self.sim_height)), dtype = bool)
+		self.static_obs_poses = [] #for visualization
 		for i in range(5):
 			for j in range(3):
-				self.static_obs[0][i + 12][j + 14] = True
-				sim_pose = Point()
-				sim_pose.x = i + 12
-				sim_pose.y = j + 14
-				sim_pose.z = 0.0
-				(sim_pose.x, sim_pose.y) = self.sim_to_real_coord((sim_pose.x, sim_pose.y))
-				self.static_obs_poses.append(sim_pose) #Now it's the real pose though
+				for k in range(3):
+					self.static_obs[0][i + 12][j + 14][k] = True
+					sim_pose = Point()
+					sim_pose.x = i + 12
+					sim_pose.y = j + 14
+					sim_pose.z = k
+					(sim_pose.x, sim_pose.y) = self.sim_to_real_coord((sim_pose.x, sim_pose.y))
+					self.static_obs_poses.append(sim_pose) #Now it's the real pose though
 
 		rospy.spin()
 
 
 	def load_parameters(self):
 		self.human_goals = [(4, 17)] 
-		self.num_robots = 3
-		self.robot_starts = [(5, 5), (5, 20), (20, 15)]
-		self.robot_goals = [[23, 23], [8, 2], [17, 4]] 
+		self.num_robots = 1
+		self.robot_starts = [(5, 5, 3), (5, 20, 3), (20, 15, 3)]
+		self.robot_goals = [[23, 23, 3], [8, 2, 3], [17, 4, 3]] 
 
 		# Sim grid is only positive (x, y)
 		self.sim_length = 26 #x-direction
 		self.sim_width = 26 #y-direction
+		self.sim_height = 10
 		# Real grid dimensions
 		self.real_length = 10
 		self.real_width = 10
@@ -73,9 +73,9 @@ class STP_Human:
 		self.collision_threshold = 0.01
 		self.tracking_error_bounds = [0, 0, 0] #tracking_error_bounds[i] is the teb planner i plans with
 
-		self.possible_planners = [astar_STP_human.AStarPlanner(self.robot_starts[0], self.sim_length, self.sim_width, self.tracking_error_bounds[0]), \
-							astar_STP_human.AStarPlanner(self.robot_starts[1], self.sim_length, self.sim_width, self.tracking_error_bounds[1]), \
-							astar_STP_human.AStarPlanner(self.robot_starts[2], self.sim_length, self.sim_width, self.tracking_error_bounds[2])]
+		self.possible_planners = [astar_STP_human.AStarPlanner(self.robot_starts[0], self.sim_length, self.sim_width, self.sim_height, self.tracking_error_bounds[0]), \
+							astar_STP_human.AStarPlanner(self.robot_starts[1], self.sim_length, self.sim_width, self.sim_height, self.tracking_error_bounds[1]), \
+							astar_STP_human.AStarPlanner(self.robot_starts[2], self.sim_length, self.sim_width, self.sim_height, self.tracking_error_bounds[2])]
 		self.planners = []
 		for i in range(self.num_robots):
 			self.planners.append(self.possible_planners[i])
@@ -89,8 +89,8 @@ class STP_Human:
 
 	def robot_follow_plan(self, planner, traj, dt):
 		for point in traj:
-			if point[2] < dt:
-				planner.curr_pos = (point[0], point[1])
+			if point[3] < dt:
+				planner.curr_pos = (point[0], point[1], point[2])
 			else:
 				break
 
@@ -98,20 +98,21 @@ class STP_Human:
 	def traj_to_obmap(self, robot_traj, robot_size, tracking_error_bound): #obmap contains True at (x, y) if there is an obstacle at (x, y)
 		obmap = {}
 		for k in range(len(robot_traj)):
-			time_stamp = robot_traj[k][2]
+			time_stamp = robot_traj[k][3]
 			obmap[time_stamp] = []
-			for i in range(self.sim_length):
-				row = [False for j in range(self.sim_width)]
-				obmap[time_stamp].append(row)
+			obmap[time_stamp] = [[[False for z in range(self.sim_height)] \
+				for y in range(self.sim_width)] for x in range(self.sim_length)]
 			x = robot_traj[k][0]
 			y = robot_traj[k][1]
+			z = robot_traj[k][2]
 			radius = math.ceil(robot_size + tracking_error_bound)
-			rectangular_radius = math.sqrt(radius**2 + radius**2) #rectangle that circumscribes the circle defined by teb 
-			for row in range(len(obmap[time_stamp])):
-				for col in range(len(obmap[time_stamp][0])):
-					dist_to_node = math.sqrt((row - x)**2 + (col - y)**2)
-					if dist_to_node <= rectangular_radius:
-						obmap[time_stamp][row][col] = True
+			cube_radius = math.sqrt(radius**2 + radius**2 + radius**2) #cube that circumscribes the sphere defined by teb 
+			for i in range(len(obmap[time_stamp])):
+				for j in range(len(obmap[time_stamp][0])):
+					for k in range(len(obmap[time_stamp][0][0])):
+						dist_to_node = math.sqrt((i - x)**2 + (j - y)**2 + (k - z)**2)
+						if dist_to_node <= cube_radius:
+							obmap[time_stamp][i][j][k] = True
 
 		return obmap
 
@@ -119,6 +120,7 @@ class STP_Human:
 	def sim_to_real_coord(self, sim_coord):
 		return (sim_coord[0] * (float(self.real_length)/self.sim_length) + self.real_lower[0], \
 			sim_coord[1] * (float(self.real_width)/self.sim_width) + self.real_lower[1])
+
 
 	def real_to_sim_coord(self, real_coord):
 		return ((real_coord[0] - self.real_lower[0]) * (float(self.sim_length)/self.real_length), \
@@ -162,7 +164,6 @@ class STP_Human:
 		marker.color.b = b
 		marker.color.a = alpha
 		marker.points = self.static_obs_poses
-		
 
 		return marker
 
@@ -203,10 +204,12 @@ class STP_Human:
 		"""
 		Converts occugrid message into structure
 		"""
-		self.occupancy_grids = [None]*self.fwd_tsteps
+		occupancy_grids = [None]*self.fwd_tsteps
 
 		for i, grid in enumerate(msg.gridarray):
-			self.occupancy_grids[i] = grid.data
+			occupancy_grids[i] = grid.data
+
+		return occupancy_grids
 
 
 
@@ -214,6 +217,9 @@ class STP_Human:
 		x = msg.x
 		y = msg.y
 		(x, y) = self.real_to_sim_coord((x, y))
+		#occupancy_grids = self.from_ROSMsg(msg)
+		#occupancy_grids_2D = [np.reshape(grid, (self.sim_length, self.sim_width)) for grid in occupancy_grids]
+
 		pose0 = Point()
 		pose1 = Point()
 		pose2 = Point()
@@ -224,21 +230,21 @@ class STP_Human:
 		if self.num_robots >= 1:
 			pose0.x = self.planners[0].curr_pos[0]
 			pose0.y = self.planners[0].curr_pos[1]
-			pose0.z = 0.0
+			pose0.z = self.planners[0].curr_pos[2]
 			rospy.loginfo(pose0)
 			self.pub0.publish(pose0)
 			self.marker_pub0.publish(self.pose_to_marker(pose0, 0, 0, 1, 1))
 		if self.num_robots >= 2:
 			pose1.x = self.planners[1].curr_pos[0]
 			pose1.y = self.planners[1].curr_pos[1]
-			pose1.z = 0.0
+			pose1.z = self.planners[1].curr_pos[2]
 			rospy.loginfo(pose1)
 			self.pub1.publish(pose1)
 			self.marker_pub1.publish(self.pose_to_marker(pose1, 0, 1, 0, 1))
 		if self.num_robots >= 3:
 			pose2.x = self.planners[2].curr_pos[0]
 			pose2.y = self.planners[2].curr_pos[1]
-			pose2.z = 0.0
+			pose2.z = self.planners[2].curr_pos[2]
 			rospy.loginfo(pose2)
 			self.pub2.publish(pose2)
 			self.marker_pub2.publish(self.pose_to_marker(pose2, 1, 1, 1, 1))
@@ -251,15 +257,25 @@ class STP_Human:
 			
 			for i in range(len(self.planners)):
 				rospy.loginfo("Robot " + str(i) + " is at (" + str(self.planners[i].curr_pos[0]) + \
-						"," + str(self.planners[i].curr_pos[1]) + ") at time " + str(self.global_time))
+						"," + str(self.planners[i].curr_pos[1]) + "," + str(self.planners[i].curr_pos[2]) + \
+						") at time " + str(self.global_time))
 
 	
 			(occupancy_grids, beta_occu, dest_beta_prob) = st.infer_joint(self.gridworld, 
 					self.dest_list, self.betas, T=self.fwd_tsteps, use_gridless=True, traj=self.obstacle_traj, verbose_return=True)
-			occupancy_grids_2D = [np.reshape(grid, (self.sim_length, self.sim_width)) for grid in occupancy_grids] 
+			occupancy_grids_2D = [np.reshape(grid, (self.sim_length, self.sim_width)) for grid in occupancy_grids]
+			occupancy_grids_3D = {}
+			for t in range(self.fwd_tsteps):
+				occupancy_grids_3D[t] = np.zeros((self.sim_length, self.sim_width, self.sim_height))
+				grid = occupancy_grids_2D[:][:][t]
+				for i in range(self.sim_length):
+					for j in range(self.sim_width):
+						for k in range(self.sim_height):
+							if k <= 2:
+								occupancy_grids_3D[t][i][j][k] = grid[i][j]
+			
 
-
-			obmaps = [occupancy_grids_2D, self.static_obs]
+			obmaps = [occupancy_grids_3D, self.static_obs]
 			robot_trajs = []
 			robot0_traj = self.planners[0].plan_traj(self.robot_goals[0], obmaps, self.collision_threshold)
 			
@@ -270,6 +286,8 @@ class STP_Human:
 			#	traj_str += str(point[1])
 			#	traj_str += ","
 			#	traj_str += str(point[2])
+			#	traj_str += " "
+			#	traj_str += str(point[3])
 			#	traj_str += " "
 			#self.debugger_pub.publish(traj_str)
 			
@@ -289,7 +307,6 @@ class STP_Human:
 
 
 			
-		
 
 if __name__ == '__main__':
     try:
--- astar_STP_human.py
+++ astar_STP_human.py
@@ -9,54 +9,64 @@ from std_msgs.msg import String
 
 class Node:
 
-    def __init__(self, x, y, cost, pind, tstamp):
+    def __init__(self, x, y, z, cost, pind, tstamp):
         self.x = x
         self.y = y
+        self.z = z
         self.cost = cost
         self.pind = pind
         self.tstamp = tstamp
 
     def __str__(self):
-        return str(self.x) + "," + str(self.y) + "," + str(self.cost) + "," + str(self.pind) + "," + str(self.tstamp)
+        return str(self.x) + "," + str(self.y) + "," + str(self.z) + "," + \
+            str(self.cost) + "," + str(self.pind) + "," + str(self.tstamp)
 
 
 class AStarPlanner:
     
-    def __init__(self, start, grid_length, grid_width, tracking_error_bound, grid_reso = 1, robot_size = 0.5):
+    def __init__(self, start, grid_length, grid_width, grid_height, tracking_error_bound, \
+                        grid_reso = 1, robot_size = 0.5, speed=3):
         self.start = start
         self.grid_length = grid_length #x direction
         self.grid_width = grid_width #y direction
+        self.grid_height = grid_height
         self.tracking_error_bound = tracking_error_bound
 
         self.grid_reso = 1
         self.robot_size = robot_size
+        self.speed = speed
         self.curr_pos = start
 
         self.debugger_pub = rospy.Publisher('planner_debugger', String, queue_size=10)
 
 
     def plan_traj(self, goal, obmaps, collision_threshold): #obmaps contains one 3D array and num_robots amount of dictionaries with 2D arrays as values
+        self.debugger_pub.publish("Planning")
         print("Planning")
-        nstart = Node(round(self.curr_pos[0]/self.grid_reso), round(self.curr_pos[1]/self.grid_reso), 0.0, -1, 0.0)
-        motion = self.get_motion_model()
+        nstart = Node(round(self.curr_pos[0]/self.grid_reso), round(self.curr_pos[1]/self.grid_reso), \
+            round(self.curr_pos[2]/self.grid_reso), 0.0, -1, 0.0)
+        #motion = self.get_motion_model()
 
         openset, closedset = dict(), dict()
-        openset[self.calc_index(nstart, self.grid_length, 0, 0)] = nstart
+        openset[self.calc_index(nstart)] = nstart
 
         gx = goal[0]
         gy = goal[1]
+        gz = goal[2]
 
+        start = rospy.Time().now().secs
         while 1:
             #for num in openset:
             #    print(openset[num].x, openset[num].y)
             c_id = min(
-                openset, key=lambda o: openset[o].cost + self.calc_h(gx, gy, openset[o].x, openset[o].y))
+                openset, key=lambda o: openset[o].cost + self.calc_h(gx, gy, gz, openset[o].x, openset[o].y, openset[o].z))
             current = openset[c_id]
             print(current)
 
-            if current.x == gx and current.y == gy:
+            if current.x == gx and current.y == gy and current.z == gz:
                 print("Found goal")
-                ngoal = Node(round(gx/self.grid_reso), round(gy/self.grid_reso), current.cost, current.pind, current.tstamp)
+                ngoal = Node(round(gx/self.grid_reso), round(gy/self.grid_reso), round(gz/self.grid_reso), \
+                    current.cost, current.pind, current.tstamp)
                 break
 
             # Remove the item from the open set
@@ -65,53 +75,63 @@ class AStarPlanner:
             closedset[c_id] = current
 
             # expand search grid based on motion model
-            for i in range(len(motion)):
-                node = Node(current.x + motion[i][0], current.y + motion[i][1],
-                            current.cost + motion[i][2], c_id, current.tstamp + (motion[i][2]/motion[i][3]))
-                
-                n_id = self.calc_index(node, self.grid_length, 0, 0)
-                
-                if not self.verify_node(node, obmaps, 0, 0, self.grid_length, self.grid_width, collision_threshold):
-                    continue
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    for k in [-1, 0, 1]:
+                        node = Node(current.x + i, current.y + j, current.z + k, \
+                            current.cost + self.calculate_motion_cost(i, j, k), c_id, \
+                            current.tstamp + 1.0/self.speed)
+                        n_id = self.calc_index(node)
 
-                if n_id in closedset:
-                    continue
-                # Otherwise if it is already in the open set
-                if n_id in openset:
-                    if openset[n_id].cost > node.cost:
-                        openset[n_id].cost = node.cost
-                        openset[n_id].pind = c_id
-                else:
-                    openset[n_id] = node
+                        if not self.verify_node(node, obmaps, 0, 0, 0, self.grid_length, \
+                            self.grid_width, self.grid_height, collision_threshold):
+                           continue
 
-        rx, ry, t = self.calc_final_traj(ngoal, closedset, self.grid_reso) #this traj is reversed in time
-        return [(rx[len(t) - 1 - i], ry[len(t) - 1 - i], t[len(t) - 1 - i]) for i in range(len(t))]
+                        if n_id in closedset:
+                            continue
+                        # Otherwise if it is already in the open set
+                        if n_id in openset:
+                            if openset[n_id].cost > node.cost:
+                                openset[n_id].cost = node.cost
+                                openset[n_id].pind = c_id
+                        else:
+                            openset[n_id] = node
+
+        end = rospy.Time().now().secs
+        self.debugger_pub.publish(str(end-start))
+
+
+        rx, ry, rz, t = self.calc_final_traj(ngoal, closedset, self.grid_reso) #this traj is reversed in time
+        return [(rx[len(t) - 1 - i], ry[len(t) - 1 - i], rz[len(t) - 1 - i], \
+            t[len(t) - 1 - i]) for i in range(len(t))]
 
 
         
     def calc_final_traj(self, ngoal, closedset, grid_reso):
         # generate final course
-        rx, ry, t = [ngoal.x * grid_reso], [ngoal.y * grid_reso], [ngoal.tstamp]
+        rx, ry, rz, t = [ngoal.x * grid_reso], [ngoal.y * grid_reso], [ngoal.z * grid_reso], \
+            [ngoal.tstamp]
         pind = ngoal.pind
         while pind != -1:
             n = closedset[pind]
             rx.append(n.x * grid_reso)
             ry.append(n.y * grid_reso)
+            rz.append(n.z * grid_reso)
             t.append(n.tstamp)
             pind = n.pind
 
-        return rx, ry, t
+        return rx, ry, rz, t
 
 
 
-    def calc_h(self, gx, gy, x, y):
+    def calc_h(self, gx, gy, gz, x, y, z):
         w = 10.0  # weight of heuristic
-        d = w * math.sqrt((gx - x)**2 + (gy - y)**2)
+        d = w * math.sqrt((gx - x)**2 + (gy - y)**2 + (gz - z)**2)
         return d
 
 
 
-    def verify_node(self, node, obmaps, minx, miny, maxx, maxy, collision_threshold): #human obmap must constitute obmaps[0]
+    def verify_node(self, node, obmaps, minx, miny, minz, maxx, maxy, maxz, collision_threshold): #human obmap must constitute obmaps[0]
         if node.x < minx:
             return False
         elif node.y < miny:
@@ -119,6 +139,10 @@ class AStarPlanner:
         elif node.x >= maxx:
             return False
         elif node.y >= maxy:
+            return False
+        elif node.z < minz:
+            return False
+        elif node.z >= maxz:
             return False
 
         human_obmap = obmaps[0]
@@ -135,48 +159,42 @@ class AStarPlanner:
 
 
     def verify_human(self, node, obmap, collision_threshold): #returns True if node is safe
-        if node.tstamp > len(obmap) - 1: #planning time exceeds time obstacle exists
-            obstacle_grid = obmap[len(obmap) - 1]
+        times = list(obmap.keys())
+        if node.tstamp > times[len(times) - 1]: #planning time exceeds time obstacle exists
+            last_time = times[len(times) - 1]
+            obstacle_grid = obmap[last_time]
             if sum(self.integrator(node, obstacle_grid)) >= collision_threshold:
                 return False
             else:
                 return True
         else:
-            for i in range(len(obmap)): #planning for a time that exists exactly in the obmap
+            for i in range(len(times)): #planning for a time that exists exactly in the obmap
                 eps = 0.001
-                if np.abs(node.tstamp - i) < eps:
-                    obstacle_grid = obmap[i]
+                if np.abs(node.tstamp - times[i]) < eps:
+                    obstacle_grid = obmap[times[i]]
                     if sum(self.integrator(node, obstacle_grid)) >= collision_threshold:
                         return False
                     else:
                         return True
 
 
-        prev_t = int(math.floor(node.tstamp)) #planning for a time that is in between time stamps in the obmap: interpolation
-        next_t = int(math.ceil(node.tstamp))
+        prev_t = 0  #planning for a time that is in between time stamps in the obmap
+        next_t = 0
+        for i in range(len(times) - 1):
+            if times[i] < node.tstamp and times[i + 1] > node.tstamp:
+                prev_t = times[i]
+                next_t = times[i + 1]
         low_grid = obmap[prev_t]
         high_grid = obmap[next_t]
-        interpolated_obstacle_grid = np.zeros((self.grid_length, self.grid_width))
+        interpolated_obstacle_grid = np.zeros((self.grid_length, self.grid_width, self.grid_height))
         for i in range(len(low_grid)):
             for j in range(len(low_grid[0])):
-                prev = low_grid[i][j]
-                next = high_grid[i][j]
-                curr = prev + (next - prev) * ((node.tstamp - prev_t) / (next_t - prev_t))
-                if i == 8 and j == 9:
-                    self.pub.publish(str(prev) + " " + str(next) + " " + str(curr))
-                interpolated_obstacle_grid[i][j] = curr
-
-        #low_grid = np.reshape(low_grid, self.grid_length * self.grid_width)
-        #high_grid = np.reshape(high_grid, self.grid_length * self.grid_width)
-        #interpolated_obstacle_grid = np.zeros(self.grid_length * self.grid_width)
-        #for i in range(self.grid_length * self.grid_width):
-        #    prev = low_grid[i]
-        #   next = high_grid[i]
-        #    curr = prev + (next - prev) * ((node.tstamp - prev_t) / (next_t - prev_t))
-        #    interpolated_obstacle_grid[i] = curr
-
-        #interpolated_obstacle_grid = np.reshape(interpolated_obstacle_grid, (self.grid_length, self.grid_width))
-        
+                for k in range(len(low_grid[0][0])):
+                    prev = low_grid[i][j][k]
+                    next = high_grid[i][j][k]
+                    curr = prev + (next - prev) * ((node.tstamp - prev_t) / (next_t - prev_t))
+                    interpolated_obstacle_grid[i][j][k] = curr
+ 
         if sum(self.integrator(node, interpolated_obstacle_grid)) >= collision_threshold:
             return False
         else:
@@ -222,32 +240,32 @@ class AStarPlanner:
     def integrator(self, node, obstacle_grid): #returns obstacle grid cells to sum over
         integrate_over = []
         radius = math.ceil(self.robot_size + self.tracking_error_bound)
-        rectangular_radius = math.sqrt(radius**2 + radius**2) #rectangle that circumscribes the circle defined by teb
-        for row in range(len(obstacle_grid)):
-            for col in range(len(obstacle_grid[0])):
-                dist_to_node = math.sqrt((row - node.x)**2 + (col - node.y)**2)
-                if dist_to_node <= rectangular_radius:
-                    integrate_over.append(obstacle_grid[row][col])
+        cube_radius = math.sqrt(radius**2 + radius**2 + radius**2) #cube that circumscribes the sphere defined by teb
+        for i in range(len(obstacle_grid)):
+            for j in range(len(obstacle_grid[0])):
+                for k in range(len(obstacle_grid[0][0])):
+                    dist_to_node = math.sqrt((i - node.x)**2 + (j - node.y)**2 + (k - node.z)**2)
+                    if dist_to_node <= cube_radius:
+                        integrate_over.append(obstacle_grid[i][j][k])
 
         return integrate_over
         
 
 
-    def calc_index(self, node, xwidth, xmin, ymin):
-        return (node.y - ymin) * xwidth + (node.x - xmin)
+    #def calc_index(self, node, xwidth, xmin, ymin):
+    #    return (node.y - ymin) * xwidth + (node.x - xmin)
+
+    def calc_index(self, node):
+        return (node.x * 100) + (node.y * 10) + node.z
+
+
+    def calculate_motion_cost(self, dx, dy, dz):
+        return math.sqrt(math.fabs(dx) + math.fabs(dy) + math.fabs(dz))
+
+
+    def calculate_motion_speed(self, dx, dy, dz):
+        return math.sqrt(math.fabs(dx) + math.fabs(dy) + math.fabs(dz)) * self.speed
 
 
 
-    def get_motion_model(self):
-        # dx, dy, cost, speed
-        motion = [[1, 0, 1.0, 3],
-                  [0, 1, 1.0, 3],
-                  [-1, 0, 1.0, 3],
-                  [0, -1, 1.0, 3],
-                  [-1, -1, math.sqrt(2), math.sqrt(18)],
-                  [-1, 1, math.sqrt(2), math.sqrt(18)],
-                  [1, -1, math.sqrt(2), math.sqrt(18)],
-                  [1, 1, math.sqrt(2), math.sqrt(18)]]
-
-        return motion
 
